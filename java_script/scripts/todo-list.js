cdead4d3b9481545717bdfbbf8726da3b997436e
siddhardhareddymuram <siddhardhareddymuram@gmail.com>
2026-01-22 16:16:53 +0530

    Add the starting files

--- java_script/scripts/todo-list.js
+++ java_script/scripts/todo-list.js
@@ -1,5 +1,13 @@
-const todolist=[];
-
+const todolist=['make dinner', 'eat dinner', 'make EVS report and PPT'];
+let todoListHTML='';
+for(let i=0;i<todolist.length;i++ ){
+   const todo=todolist[i];
+   const html=`<p>${todo}<\p>`;
+   todoListHTML+=html;
+}
+console.log(todoListHTML);
+document.querySelector('.js-name-input');
+.innerHTML=todoListHTML;
 function addTodo(){
    const inputelement= document.querySelector('.js-name-input');
    const name=inputelement.value;
